65733f2e7101ce7a28a35b26fe4b3580199f2a66
Technolords <mike@technolords.net>
2017-06-29 17:21:05 +0200

    Initial import of custom tcp handler for one way (send only)

---
+++ src/main/java/org/apache/jmeter/protocol/tcp/sampler/TCPOnewayClientImpl.java
@@ -0,0 +1,34 @@
+package org.apache.jmeter.protocol.tcp.sampler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TCPOnewayClientImpl extends TCPClientImpl {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TCPOnewayClientImpl.class);
+
+    public TCPOnewayClientImpl() {
+        LOGGER.debug("Custom tcp client handler created...");
+    }
+
+    @Override
+    public void write(OutputStream outputStream, InputStream inputStream) throws IOException {
+        LOGGER.debug("Write called...");
+        super.write(outputStream, inputStream);
+    }
+
+    @Override
+    public void write(OutputStream outputStream, String s) throws IOException {
+        LOGGER.debug("Write called...");
+        super.write(outputStream, s);
+    }
+
+    @Override
+    public String read(InputStream inputStream) throws ReadException {
+        LOGGER.debug("Read called...");
+        return "";
+    }
+}
